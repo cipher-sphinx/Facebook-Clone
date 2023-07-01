79415b2fca217549421680a8419272be4a190559
new-username-now <84344132+new-username-now@users.noreply.github.com>
2023-07-01 20:32:28 +0530

    facebook-clone-using-javascript-react

--- facebook-clone-using-javascript-react/src/components/Container.jsx
+++ facebook-clone-using-javascript-react/src/components/Container.jsx
@@ -16,57 +16,9 @@ function Container() {
         <div className="title">Recent Logins</div>
         <div className="subtitle">Click your picture or add an account.</div>
         <AccountCard />
-        {/* <div id="cards">
-          <div id="card-1">
-            <div id="cancel">x</div>
-            <img
-              id="card-1-img"
-              src="images/Profile-Picture.jpg"
-              alt="profile-picture-1"
-            />
-            <div class="username">Khakwaozi</div>
-          </div>
-
-          <div id="card-1">
-            <div id="cancel">x</div>
-            <img
-              id="card-1-img"
-              src="images/profile-picture.jpg"
-              alt="profile-picture-1"
-            />
-            <div class="username">Menzeekhi</div>
-          </div>
-
-          <div id="card-2">
-            <img
-              id="card-2-img"
-              src="images/add-account.jpg"
-              alt="add-account"
-            />
-            <div class="username" id="add-account">
-              Add Account
-            </div>
-          </div>
-        </div> */}
       </div>
 
       <LoginForm />
-      {/* <div className="login">
-        
-        <input type="email" placeholder="Email or Phone Number" />
-        <input type="password" placeholder="Password" />
-        <input type="button" value="Log In" id="login-button" />
-        <a href="#">Forgot Password?</a>
-        <hr style="width: 95%;" />
-        <input
-          type="button"
-          value="Create New Account"
-          id="create-new-account"
-        />
-        <p>
-          <strong>Create a Page</strong> for a Celebrity, Brand or Business
-        </p>
-      </div> */}
     </div>
   );
 }
